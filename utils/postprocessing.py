d684aa09e2169868e29426562106d2045454c9aa
dipeshtate24 <dipeshtate24@gmail.com>
2026-05-23 23:10:09 +0530

    updated postprocessing file

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -1,43 +1,37 @@
 import cv2
 import numpy as np
-from ultralytics import YOLO
 from collections import defaultdict
 
-def video_postprocessing(video_path):
-
-    cap = cv2.VideoCapture(video_path)
+def map_tracking(results, frame):
 
     track_history = defaultdict(lambda:[])
 
-    while cap.isOpened():
+    result = results[0]
         
-        ret, frame = cap.read()
-
-        if ret:
-        
-            result = model.track(frame, persist=True, classes =[0])[0]
             
-            # Get the boxes and track IDs
-            if result.boxes and result.boxes.is_track:
-                boxes = result.boxes.xywh.cpu()
-                track_ids = result.boxes.id.int().cpu().tolist()
+    # Get the boxes and track IDs
+    if result.boxes is not None and result.boxes.id is not None:
+        boxes = result.boxes.xywh.cpu()
+        track_ids = result.boxes.id.int().cpu().tolist()
 
-                # Visualize the result on the frame
-                frame = result.plot()
+        # Visualize the result on the frame
+        annoted_frame = result.plot()
 
-                # Plot the trace
-                for box, track_id in zip(boxes, track_ids):
-                    x, y, w, h = box
-                    track = track_history[track_id] 
-                    track.append(float(x), float(y)) # x, y center point
+        # Plot the trace
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            
+            track = track_history[track_id] 
+            
+            track.append((float(x), float(y))) # x, y center point
 
-                    if len(track) > 30: # retain 30 tracks for 30 frames
+            if len(track) > 30: # retain 30 tracks for 30 frames
                     
-                        track.pop(0)
+                track.pop(0)
 
-                    # Draw the tracking lines
-                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                    cv2.polylines(frame, [points], isClosed=False, color =(230, 230, 230), thickness=5)
+                # Draw the tracking lines
+                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(frame, [points], isClosed=False, color =(230, 230, 230), thickness=5)
 
             # Display the annoted frame
             cv2.imshow('Person Tracking', frame)
