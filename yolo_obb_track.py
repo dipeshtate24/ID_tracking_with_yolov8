4959df4c7a6824e6e935a2e1d3c626207bebd590
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-06-29 23:44:11 +0530

    Update yolo_obb_track.py

--- yolo_obb_track.py
+++ yolo_obb_track.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt") 
 
-results = model.track(source=r"C:\Users\Dipesh\Documents\yolo_object_detection_tracking\testing_video\1625968-hd_1920_1080_25fps.mp4", show=True, tracker="bytetrack.yaml")
+results = model.track(source=".\testing_video\1625968-hd_1920_1080_25fps.mp4", show=True, tracker="bytetrack.yaml")
 print(results)
